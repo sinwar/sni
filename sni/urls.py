b0b2e521a950e03a4739c6280726b5b5d4c504ee
sinwar <premsinwar4@gmail.com>
2016-05-14 00:39:31 +0530

    change for profile pic

--- sni/urls.py
+++ sni/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 from .views import SignupView, ProfileView, ProView
@@ -17,4 +17,6 @@ urlpatterns = [
     url(r"^account/", include("account.urls")),
 ]
 
+
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

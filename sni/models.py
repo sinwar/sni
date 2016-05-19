870269edf4768f57753c98288a728c0ab6d59afe
sinwar <premsinwar4@gmail.com>
2016-05-19 05:37:09 +0530

    multiple items selection

--- sni/models.py
+++ sni/models.py
@@ -2,16 +2,7 @@ from django.conf import settings
 from django.db import models
 import datetime
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, primary_key = True)
-    first_name = models.TextField(default=" ")
-    last_name = models.TextField(default=" ")
-    image = models.ImageField(default=" ", upload_to = settings.MEDIA_ROOT)
-    def __str__(self):
-    	return "{0}".format(self.user)
-
 class addThing(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, primary_key = True)
     itemname = models.CharField(default=" ", max_length=30)
     details = models.TextField(default=" ")
     rate = models.IntegerField(default=" ")
@@ -19,3 +10,13 @@ class addThing(models.Model):
     datetime = models.DateTimeField(default=datetime.datetime.now())
     def __str__(self):
         return "{0}".format(self.itemname)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, primary_key = True)
+    first_name = models.TextField(default=" ")
+    last_name = models.TextField(default=" ")
+    image = models.ImageField(default=" ", upload_to = settings.MEDIA_ROOT)
+    items = models.ForeignKey(addThing, on_delete = models.CASCADE, null = True, blank =True)
+    def __str__(self):
+    	return "{0}".format(self.user)
+
